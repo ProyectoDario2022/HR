c68b5926c91b10a9e201adc02088e422a9d8c221
ProyectoDario <dario103d@yahoo.com.ar>
2022-11-14 00:29:39 -0300

    recupero de contraseña y mails ok

--- HR.API/Helpers/UserHelper.cs
+++ HR.API/Helpers/UserHelper.cs
@@ -91,6 +91,11 @@ namespace HR.API.Helpers
             return await _userManager.GenerateEmailConfirmationTokenAsync(user);
         }
 
+        public async Task<string> GeneratePasswordResetTokenAsync(User user)
+        {
+            return await _userManager.GeneratePasswordResetTokenAsync(user);
+        }
+
         public async Task<User> GetUserAsync(string email)
         {
             return await _context.Users
@@ -123,6 +128,11 @@ namespace HR.API.Helpers
         public async Task LogoutAsync()
         {
             await _signInManager.SignOutAsync();
+        }
+
+        public async Task<IdentityResult> ResetPasswordAsync(User user, string token, string password)
+        {
+            return await _userManager.ResetPasswordAsync(user, token, password);
         }
 
         public async Task<IdentityResult> UpdateUserAsync(User user)
